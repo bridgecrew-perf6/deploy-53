0e6d6b852fd7b1730b99d36f7b0e8b601b3645fb
hello-liang <liangshuaibing@outlook.com>
2022-06-16 17:13:23 +0800

    final change to 24

--- collect_data.py
+++ collect_data.py
@@ -32,7 +32,7 @@ def CollectData():
 
     # codec = cv2.VideoWriter_fourcc((*'png '))
     # codec = cv2.VideoWriter_fourcc(*'mjpg')
-
+#
     fps = 24.0  # 指定写入帧率为30
     frameSize = (640, 480)  # 指定窗口大小
     # 创建 VideoWriter对象
